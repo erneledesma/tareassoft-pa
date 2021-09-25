851bfee4d67ef9055a6d22c1b5143ed4ed40e665
Ernesto Ledesma <eeledesma@teco.com.ar>
2021-09-25 12:35:12 -0300

    proyecto final completo

--- src/App.js
+++ src/App.js
@@ -6,8 +6,26 @@ import Tarea from './components/Tarea'
 
 function App() {
 
+  // Tareas en el localStorage 
+  let tareasIniciales = JSON.parse(localStorage.getItem('tareas'));
+  if(!tareasIniciales){
+    tareasIniciales = [];
+  }
+
   // Arreglo de tareas
-  const [ tareas, guardarTareas ] = useState([]);
+  const [ tareas, guardarTareas ] = useState(tareasIniciales);
+
+  // UseEffect para realizar ciertas operaciones cuando el State cambia 
+    useEffect(() => {
+      let tareasIniciales = JSON.parse(localStorage.getItem('tareas'));
+
+    if(tareasIniciales) {
+      localStorage.setItem('tareas', JSON.stringify(tareas))
+    }else {
+      localStorage.setItem('tareas', JSON.stringify([]))
+    }
+    
+    },[tareas])
 
   // Funcion que tome las tareas y agregue una nueva
   const crearTarea = tarea => {
@@ -19,14 +37,10 @@ function App() {
 
   // funcion que elimina las tareas
   const eliminarTarea = id => {
-    const nuavasTareas = tareas.filter( tarea => tarea.id === id )
+    const nuavasTareas = tareas.filter( tarea => tarea.id !== id )
     guardarTareas(nuavasTareas);
   }
   
-  // UseEffect para realizar ciertas operaciones cuando el State cambia 
-  useEffect(() => {
-    console.log('Listo')
-  },[tareas])
 
   // mensaje 
   const titulo = tareas.length === 0 ? 'No hay tareas' : 'Administra tus tareas'
